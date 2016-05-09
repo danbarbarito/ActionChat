4dd1c0de7c89bed61afce3ab4e7ae9ac59f5426c
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-05-09 13:20:31 -0400

    Origin fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['http://52.8.41.30']
 end

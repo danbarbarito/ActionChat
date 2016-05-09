2b15d650a49a7181ae6e8ea03a3e9d2b1e6113f2
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-05-09 13:50:17 -0400

    id -> email

--- config/initializers/warden_hooks.rb
+++ config/initializers/warden_hooks.rb
@@ -1,10 +1,10 @@
 Warden::Manager.after_set_user do |user,auth,opts|
   scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = user.id
+  auth.cookies.signed["#{scope}.email"] = user.email
 end
 
 Warden::Manager.before_logout do |user, auth, opts|
   scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = nil
+  auth.cookies.signed["#{scope}.email"] = nil
   auth.cookies.signed["#{scope}.expires_at"] = nil
 end
